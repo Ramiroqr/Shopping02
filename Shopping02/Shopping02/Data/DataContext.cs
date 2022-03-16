257efc044f2b3a34178e4e8a50b8f9a875a1db5b
Ramiroqr <ramiroquisper@gmail.com>
2022-03-16 07:16:32 -0500

    Countries entiti

---
+++ Shopping02/Shopping02/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping02.Data.Entities;
+
+namespace Shopping02.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
